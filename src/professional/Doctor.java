95b7f366fb72c4fa3b8ff81944111be2b2c69c39
pseboyeng@gmail.com <pseboyeng@gmail.com>
2022-06-30 19:54:44 +0200

    added doctor class

---
+++ src/professional/Doctor.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package professional;
+
+/**
+ * @author psebo
+ *
+ */
+public final class Doctor extends MedicalProfessional {
+	private String samrcNumber;
+	private double ratePerHour;
+
+	public Doctor(String name, String surname, String hpcsaNo, String samrcNumber) {
+		super(name, surname, hpcsaNo);
+		this.samrcNumber = samrcNumber;
+
+	}
+
+	@Override
+	public void setHourlyRate(double rate) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getSamrcNumber() {
+		return samrcNumber;
+	}
+
+	public double getRatePerHour() {
+		return ratePerHour;
+	}
+
+	public boolean verifySAMRCNumber() {
+		return MedicalCouncil.MedicalCouncil.getSAMRC().equalsIgnoreCase(samrcNumber);
+	}
+
+	@Override
+	public String toString() {
+		return "Doctor { Full Name :" + getName() + " " + getSurname() + " . South_African_Medical_Research_Council"
+				+ samrcNumber + ", hourly rate : " + ratePerHour + " }\n";
+	}
+
+}
